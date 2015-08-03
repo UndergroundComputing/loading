5acc8305638c9064a8b386ffa80bacf0d532030a
UndergroundComputing <richard.kocjancic@gmail.com>
2015-08-03 00:25:54 -0400

    New repo!

---
+++ src.loading.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int repeats=80;
+	for (;repeats>0;repeats--){
+
+		system("clear");
+		printf("Loading [           ]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [#          ]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [##         ]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [###        ]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [ ###       ]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [  ###      ]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [   ###     ]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [    ###    ]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [     ###   ]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [      ###  ]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [       ### ]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [        ###]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [         ##]\n");
+		system("sleep .1");
+
+		system("clear");
+		printf("Loading [          #]\n");
+		system("sleep .1");
+		}
+
+	system("clear");
+	printf("You will never get your time back!\n");
+	return 0;
+}
