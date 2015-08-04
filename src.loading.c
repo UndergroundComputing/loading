ada74b7bb1b259b15f0351a4f4de2c5aa12dc2b0
UndergroundComputing <richard.kocjancic@gmail.com>
2015-08-04 00:02:48 -0400

    Customize the loading text with the use of args!

--- src.loading.c
+++ src.loading.c
@@ -1,65 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	char *msg=NULL;
+	if (argc < 2) {
+		msg=strdup("Loading");
+	}
+	if (argc > 1) {
+		msg=argv[1];
+	}
+
 	int repeats=80;
 	for (;repeats>0;repeats--){
 
 		system("clear");
-		printf("Loading [           ]\n");
+		printf("%s [           ]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [#          ]\n");
+		printf("%s [#          ]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [##         ]\n");
+		printf("%s [##         ]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [###        ]\n");
+		printf("%s [###        ]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [ ###       ]\n");
+		printf("%s [ ###       ]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [  ###      ]\n");
+		printf("%s [  ###      ]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [   ###     ]\n");
+		printf("%s [   ###     ]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [    ###    ]\n");
+		printf("%s [    ###    ]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [     ###   ]\n");
+		printf("%s [     ###   ]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [      ###  ]\n");
+		printf("%s [      ###  ]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [       ### ]\n");
+		printf("%s [       ### ]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [        ###]\n");
+		printf("%s [        ###]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [         ##]\n");
+		printf("%s [         ##]\n", msg);
 		system("sleep .1");
 
 		system("clear");
-		printf("Loading [          #]\n");
+		printf("%s [          #]\n", msg);
 		system("sleep .1");
 		}
 
